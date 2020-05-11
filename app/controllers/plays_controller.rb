db2de2b570266111ea34975cf510b1ee7c5f033d
Mayank Saraf <mayanksaraf04@gmail.com>
2020-05-11 16:01:07 +0530

    Added Home page with validation

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,2 +1,20 @@
 class PlaysController < ApplicationController
+  def home
+    @images = PlayImage.new
+  end
+
+  def store_images
+    @images = PlayImage.new(play_image_params)
+    if @images.save
+      redirect_to plays_home_path
+    else
+      render 'home'
+    end
+  end
+
+  private
+
+  def play_image_params
+    params.require(:play_image).permit(images: [])
+  end
 end
--- app/models/play_image.rb
+++ app/models/play_image.rb
@@ -1,3 +1,16 @@
 class PlayImage < ApplicationRecord
   has_many_attached :images
+
+  validate :acceptable_image
+
+  def acceptable_image
+    return unless images.attached?
+
+    acceptable_types = ["image/jpeg", "image/png"]
+    images.each do |image|
+      unless acceptable_types.include?(image.content_type)
+        return errors.add(:images, "must be a JPEG or PNG")
+      end
+    end
+  end
 end
